fc99c79d969a900c58322a7d2c7b0128e859a882
Mark Macdonald <mark.t.macdonald@googlemail.com>
2023-10-16 18:47:24 +0100

    Remove extension from import

--- src/docgen.ts
+++ src/docgen.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-import { DocGen } from './docgen/docgen.js';
+import { DocGen } from './docgen/docgen';
 
 const generator = new DocGen(process);
 
