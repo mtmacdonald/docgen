67b2311304b202a0d1acbd433102ee02c131d329
Mark Macdonald <mark.t.macdonald@googlemail.com>
2023-11-09 19:48:25 +0000

    Prototype using React for view templates

---
+++ src/docgen/html/web-table-of-contents.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { toHTML } from './html';
+
+const Page = ({page}) => {
+  const name = page.source.substring(0, page.source.lastIndexOf('.'));
+  const path = name + '.html';
+  return (
+    <li><a href={path}>{page.title}</a></li>
+  );
+};
+
+const Section = ({section}) => {
+  if (section?.heading === 'Extra') {
+    return null;
+  }
+  const visible = section?.heading && section?.pages;
+  return (
+   <td className="dg-tocGroup">
+     {visible && (
+       <ul>
+         <li className="dg-tocHeading">
+           {section.heading}
+         </li>
+         {section.pages.map((page, i)=> (
+           <Page key={i} page={page} />
+         ))}
+       </ul>
+     )}
+   </td>
+  )
+};
+
+const TableOfContents = ({
+  sortedPages,
+  name,
+  pdfEnabled,
+}) => {
+  let pdfName = name.toLowerCase() + '.pdf';
+  return (
+    <div>
+      <table className="unstyled">
+        {Object.values(sortedPages).map((section, i) => (
+          <Section key={i} section={section?.[0]} />
+        ))}
+        <td className="dg-tocGroup" id="dg-tocFixedColumn">
+          <ul>
+            <li>
+              <span className="w-icon dg-tocIcon" data-name="person_group" title="archive"></span>
+              <a href="ownership.html">Ownership</a>
+            </li>
+            <li>
+              <span className="w-icon dg-tocIcon" data-name="refresh" title="archive"></span>
+              <a href="release-notes.html">Release Notes</a>
+            </li>
+          </ul>
+          <div>
+            {pdfEnabled && (
+              <a
+                className="button inverted"
+                href={pdfName}
+              >
+                PDF
+              </a>
+            )}
+          </div>
+        </td>
+      </table>
+    </div>
+  );
+}
+
+export const generateWebTableOfContents = ({
+  sortedPages,
+  mainTemplate,
+  name,
+  pdfEnabled,
+}) => {
+  let $ = mainTemplate;
+  const html = toHTML(
+    <TableOfContents
+      sortedPages={sortedPages}
+      name={name}
+      pdfEnabled={pdfEnabled}
+    />
+  );
+  $('#dg-toc').html(html);
+  return $;
+};
