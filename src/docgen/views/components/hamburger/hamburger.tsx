94c3bf06d622d1cdcf6b5481a752df1735bd33d1
Mark Macdonald <mark.t.macdonald@googlemail.com>
2024-03-07 18:16:35 +0100

    Remove reduntant wrapper

--- src/docgen/views/components/hamburger/hamburger.tsx
+++ src/docgen/views/components/hamburger/hamburger.tsx
@@ -20,53 +20,51 @@ export const Hamburger = ({
   const pages = Object.values(sortedPages).flat();
   const pdfName = name.toLowerCase() + '.pdf';
   return (
-    <>
-      <div id="nav-container">
-        <div className="bg"></div>
-        <div className="button" tabIndex="0">
-          <span className="icon-bar"></span>
-          <span className="icon-bar"></span>
-          <span className="icon-bar"></span>
-        </div>
-        <div id="nav-content" tabIndex="0">
-          <div className="dg-hamburger-menu-content">
-            <div>
-              {pdfEnabled && (
-                <a
-                  className="button whiteInverted"
-                  style={{textDecoration: 'none'}}
-                  href={pdfName}
-                >
-                  PDF
-                </a>
-              )}
-            </div>
-            {pages.map((page, i) => (
-              <div key={i}>
-                <h2>{page.heading}</h2>
-                <ul>
-                  {page.pages.map((subPage, i) => (
-                    <Page key={i} page={subPage} />
-                  ))}
-                </ul>
-              </div>
-            ))}
-            <div>
-              <h2></h2>
+    <div id="nav-container">
+      <div className="bg"></div>
+      <div className="button" tabIndex="0">
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+      </div>
+      <div id="nav-content" tabIndex="0">
+        <div className="dg-hamburger-menu-content">
+          <div>
+            {pdfEnabled && (
+              <a
+                className="button whiteInverted"
+                style={{textDecoration: 'none'}}
+                href={pdfName}
+              >
+                PDF
+              </a>
+            )}
+          </div>
+          {pages.map((page, i) => (
+            <div key={i}>
+              <h2>{page.heading}</h2>
               <ul>
-                <li>
-                  <span className="dgIcon" data-name="users" title="ownership"></span>
-                  <a href="ownership.html">Ownership</a>
-                </li>
-                <li>
-                  <span className="dgIcon" data-name="refresh" title="release notes"></span>
-                  <a href="release-notes.html">Release Notes</a>
-                </li>
+                {page.pages.map((subPage, i) => (
+                  <Page key={i} page={subPage} />
+                ))}
               </ul>
             </div>
+          ))}
+          <div>
+            <h2></h2>
+            <ul>
+              <li>
+                <span className="dgIcon" data-name="users" title="ownership"></span>
+                <a href="ownership.html">Ownership</a>
+              </li>
+              <li>
+                <span className="dgIcon" data-name="refresh" title="release notes"></span>
+                <a href="release-notes.html">Release Notes</a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
