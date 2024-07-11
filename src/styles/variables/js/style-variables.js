fa206a84f1b9648279f81934e9b54a285619a5aa
Mark Macdonald <mark.t.macdonald@googlemail.com>
2024-07-11 19:20:41 +0100

    Lift more colors to style tokens

--- src/styles/variables/js/style-variables.js
+++ src/styles/variables/js/style-variables.js
@@ -2,5 +2,8 @@
  * Do not edit directly, this file was auto-generated.
  */
 
+export const ColorsBackground = '#ffffff';
+export const ColorsBorder = '#999999';
+export const ColorsText = '#444444';
 export const ColorsTextPrimary = '#010304';
 export const ColorsTextPrimaryHover = '#153f54';
