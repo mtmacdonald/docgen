fb27862e5d30c719e1c73c27e135f9478507082e
Mark Macdonald <mark.t.macdonald@googlemail.com>
2024-07-15 17:47:45 +0100

    Improve link styles

--- src/styles/variables/js/style-variables.js
+++ src/styles/variables/js/style-variables.js
@@ -2,10 +2,10 @@
  * Do not edit directly, this file was auto-generated.
  */
 
-export const ColorsBackground = '#ffffff';
-export const ColorsBorder = '#999999';
-export const ColorsText = '#010304';
-export const ColorsTextLink = '#010304';
+export const ColorsBackgroundLight = '#fff';
+export const ColorsBorder = '#999';
+export const ColorsTextBase = 'rgb(1, 3, 4)';
+export const ColorsTextLink = 'rgb(1, 3, 4)';
 export const ColorsTextLinkHover = '#4a72c1';
-export const ColorsTextLinkFocused = '#4b91ff';
-export const ColorsTextLinkVisited = '#4a72c1';
+export const ColorsTextLinkActive = '#4b91ff';
+export const ColorsTextLinkVisited = '#808080';
