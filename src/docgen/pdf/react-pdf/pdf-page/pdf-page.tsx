35791be0082b9d4a42026d4d4e4a73f7fe874474
Mark Macdonald <mark.t.macdonald@googlemail.com>
2024-08-05 19:29:54 +0100

    Prototype adding custom styles

--- src/docgen/pdf/react-pdf/pdf-page/pdf-page.tsx
+++ src/docgen/pdf/react-pdf/pdf-page/pdf-page.tsx
@@ -2,25 +2,60 @@ import React from 'react';
 import { Page, View, StyleSheet } from '@react-pdf/renderer';
 import Html from 'react-pdf-html';
 import { PdfFooter } from './pdf-footer/pdf-footer';
+import * as styles from '../../../../styles/variables/js/style-variables';
 
-const styles = StyleSheet.create({
+const fontSize = 10;
+
+const pdfStyleSheet = StyleSheet.create({
   page: {
     padding: '1cm',
-    fontSize: 10,
-    margin:0,
+    fontSize,
+    margin: 0,
   },
 });
+
+const styleInfo = {
+  color: styles.ColorTextInfo,
+  backgroundColor: styles.ColorBackgroundInfo,
+  borderLeft: `5px solid ${styles.ColorBorderInfo}`,
+  padding: styles.SizeMessagePadding,
+  //borderRadius: styles.SizeMessageBorderRadius,
+}
+
+const em = (em: number, relativeSize: number = fontSize) => em * relativeSize;
+
+const htmlStyleSheet = {
+  p: {
+    color: styles.ColorText
+  },
+  h1: {
+    fontSize: em(1.4),
+    marginVertical: em(0.2, em(2)),
+    fontWeight: 'bold',
+  },
+  blockquote: styleInfo,
+  ['.w-information']: styleInfo,
+  pre: {
+    //padding: 12px,
+    //background-color: #f3f3f3,
+    borderLeft: `1px solid ${styles.ColorBorder}`,
+    //line-height: 14px;
+    //border-radius: 6px;
+  }
+};
 
 export const PdfPage = ({
   page,
   parameters
 }) => {
   return (
-    <Page size="A4" style={styles.page}>
+    <Page size="A4" style={pdfStyleSheet.page}>
       <View>
         <
-          // @ts-ignore
           Html
+          resetStyles
+          style={{ fontSize }}
+          stylesheet={htmlStyleSheet}
         >
           {page}
         </Html>
