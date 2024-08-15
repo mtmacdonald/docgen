ad930d09cbcea042ed1aba0d949f7904de6f0572
Mark Macdonald <mark.t.macdonald@googlemail.com>
2024-08-15 17:46:29 +0100

    PDF playgroud - add JSX inspector

--- pdf-test-playground/src/app/app.tsx
+++ pdf-test-playground/src/app/app.tsx
@@ -1,13 +1,14 @@
+import { ObjectInspector } from 'react-inspector';
 import { Document, Page, PDFViewer } from '@react-pdf/renderer';
-//import { renderHtml } from 'react-pdf-html';
-import { Pdf /*, html */ } from '../document.tsx';
+import { renderHtml } from 'react-pdf-html';
+import { Pdf, html } from '../document.tsx';
 import styles from './app.module.css';
 
-const App = () => {
+export const App = () => {
   return (
     <div className={styles.container}>
       <div className={styles.left}>
-        Placeholder
+        <ObjectInspector data={renderHtml(html)} />
       </div>
       <div className={styles.right}>
         <PDFViewer>
@@ -21,5 +22,3 @@ const App = () => {
     </div>
   );
 };
-
-export default App
--- pdf-test-playground/src/main.tsx
+++ pdf-test-playground/src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import App from './app/app.tsx'
+import { App } from './app/app.tsx'
 import '../../src/styles/normalize.css';
 import './index.css'
 
