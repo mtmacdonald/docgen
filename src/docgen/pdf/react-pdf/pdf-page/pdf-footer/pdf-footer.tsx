52fe137d9e795c0e9261d5046a9100b55df3c1c4
Mark Macdonald <mark.t.macdonald@googlemail.com>
2024-09-26 19:25:23 +0100

    Fix PDF footer styles

--- src/docgen/pdf/react-pdf/pdf-page/pdf-footer/pdf-footer.tsx
+++ src/docgen/pdf/react-pdf/pdf-page/pdf-footer/pdf-footer.tsx
@@ -2,60 +2,31 @@ import React from 'react';
 import { View, Text, StyleSheet } from '@react-pdf/renderer';
 
 const styles = StyleSheet.create({
-  // footer: {
-  //   position: 'absolute',
-  //   bottom: 0,
-  //   left: 0,
-  //   right: 0,
-  //   textAlign: 'center',
-  //   fontSize: 12,
-  //   color: 'grey',
-  // },
-  // footer: {
-  //   position: 'absolute',
-  //   bottom: 0, // Adjust as needed
-  //   left: 0,
-  //   right: 0,
-  //   textAlign: 'center',
-  //   fontSize: 10,
-  //   color: 'grey',
-  // },
   footer: {
     position: 'absolute',
-    fontSize: 8,
-    bottom: 30,
-    left: 0,
-    right: 0,
-    display: 'flex'
+    bottom: 5,
+    left: 30,
+    right: 30,
+    height: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderTopWidth: 1,
+    borderTopColor: 'black',
+    paddingTop: 10,
   },
-  footerLeft: {
-    flex: 1,
-    display: 'flex',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-  },
-  footerMiddle: {
-    flex: 1,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  footerRight: {
-    flex: 1,
-    display: 'flex',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+  column: {
+    width: '33%',
+    textAlign: 'center',
+    fontSize: 10,
   }
 });
 
 export const PdfFooter = ({parameters}) => (
   <View style={styles.footer} fixed>
-    <Text style={styles.footerLeft}>{parameters.title}</Text>
-    <Text style={styles.footerMiddle}>
-      {`© ${parameters.year} ${parameters.name}`}
-    </Text>
+    <Text style={styles.column}>{parameters.title}</Text>
+    <Text style={styles.column}>{`© ${parameters.year} ${parameters.name}`}</Text>
     <Text
-      style={styles.footerRight}
+      style={styles.column}
       render={({ pageNumber, totalPages }) => (
         `${pageNumber} / ${totalPages}`
       )}
