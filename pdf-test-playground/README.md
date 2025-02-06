639248a7bc4458cfa154b59b8684b227045afa85
Mark Macdonald <mark.t.macdonald@googlemail.com>
2025-02-06 16:07:29 +0000

    Remove testing project

--- pdf-test-playground/README.md
+++
@@ -1,3 +0,0 @@
-# PDF Test Playground
-
-This small React app is for ad-hoc testing of react-pdf, using the DocGen configuration.
--- pdf-test-playground/src/app/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import { App } from './app.tsx'
-import '../../../src/styles/normalize.css';
-import './index.css'
-
-createRoot(document.getElementById('root')!).render(
-  <App />
-);
