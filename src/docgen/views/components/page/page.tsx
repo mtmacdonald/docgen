3a5ab4a8fa59eb55aefeecdd34daca8e3912ae85
Mark Macdonald <mark.t.macdonald@googlemail.com>
2025-07-23 16:46:02 +0100

    Fix Type Checks in Linux

--- src/docgen/views/components/page/page.tsx
+++ src/docgen/views/components/page/page.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
-export const Page = ({children}) => {
+type PageProps = {
+  children?: ReactNode;
+};
+
+export const Page = ({children} : PageProps) => {
   return (
     <div
       className="page"
