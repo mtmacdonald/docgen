2f98367c0fd2938dabe4b431d36780da0b413070
Mark Macdonald <mark.t.macdonald@googlemail.com>
2025-08-05 18:09:12 +0100

    Hello world react app

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import path from 'node:path';
+
+export default defineConfig({
+  root: path.resolve(__dirname),
+  build: {
+    outDir: path.resolve(__dirname, 'docs-new'),
+  },
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      entry: 'src/docgen/views/html.tsx',
+      template: 'new-index.html',
+    }),
+  ],
+});
