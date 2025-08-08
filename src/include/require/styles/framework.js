f98c9c150e692716a423ea5536f7decff2ad26bb
Mark Macdonald <mark.t.macdonald@googlemail.com>
2025-08-08 17:05:14 +0100

    Remove dead icons code

--- src/include/require/styles/framework.js
+++ src/include/require/styles/framework.js
@@ -11,29 +11,8 @@ const injectSVG = () => {
 };
 
 /*
-  SideBar Toggle Button
-*/
-const initSideBar = () => {
-  const sideBar = document.getElementById('dgSideBar');
-  const sideBarButton = document.getElementById('dgSideBarButton');
-  const sideBarIcon = sideBarButton.querySelector('span.dgIcon');
-  sideBarButton.addEventListener('click', function () {
-    if (sideBar.classList.contains('dgSideBarCollapsed')) {
-      sideBar.classList.remove('dgSideBarCollapsed');
-      sideBarIcon.setAttribute('data-name', 'x');
-      sideBarIcon.innerHTML = w_icons['x'];
-    } else {
-      sideBar.classList.add('dgSideBarCollapsed');
-      sideBarIcon.setAttribute('data-name', 'menu-2');
-      sideBarIcon.innerHTML = w_icons['menu-2'];
-    }
-  });
-};
-
-/*
   Document Ready
 */
 document.addEventListener('DOMContentLoaded', function () {
-  initSideBar();
   injectSVG();
 });
