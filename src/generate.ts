7a90fe60c0ad1dbe5b57bfac57cb3cacde25f9f0
Mark Macdonald <mark.t.macdonald@googlemail.com>
2025-08-11 18:00:28 +0100

    Switch to @vitejs/plugin-react-oxc

--- src/generate.ts
+++ src/generate.ts
@@ -1,6 +1,6 @@
 import path from 'node:path';
 import { build, createServer } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-oxc';
 import { copyStaticFiles } from './copy-static-files.ts';
 
 export const runVite = async (command, mode) => {
