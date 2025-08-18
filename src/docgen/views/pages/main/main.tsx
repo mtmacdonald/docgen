09b6a73d49b3e0212db0144a2b6dcb756d18c12e
Mark Macdonald <mark.t.macdonald@googlemail.com>
2025-08-18 17:29:18 +0100

    Fix small regression in main page layout

--- src/docgen/views/pages/main/main.tsx
+++ src/docgen/views/pages/main/main.tsx
@@ -1,8 +1,8 @@
-import React, { useState, ReactNode } from 'react';
+import React, { useState } from 'react';
 import { WebFooter } from '../../components/footer/footer.tsx';
 import { TopBar } from '../../components/top-bar/top-bar.tsx';
 import { SideBar } from '../../components/side-bar/side-bar.tsx';
-import { Page as PageLayout } from '../../components/page/page.tsx';
+import { Page } from '../../components/page/page.tsx';
 import { PDFViewer } from '@react-pdf/renderer';
 import type { Parameters } from '../../../types.ts';
 import { Pdf } from '../../../pdf/react-pdf/react-pdf.tsx';
@@ -14,40 +14,28 @@ type MainProps = {
   pdfEnabled: boolean;
 };
 
-export const Main = ({
-  parameters,
-  sortedPages,
-  pdfEnabled,
-}: MainProps) => {
+export const Main = ({ parameters, sortedPages, pdfEnabled }: MainProps) => {
   const [showPdf, setShowPdf] = useState(false);
   const { name } = parameters;
 
   return (
     <>
       <TopBar parameters={parameters} onPdfToggle={() => setShowPdf(prev => !prev)} />
-      <SideBar
-        name={name}
-        sortedPages={sortedPages}
-        pdfEnabled={pdfEnabled}
-      />
-      <PageLayout>
-        <section id="dg-content" style={{ height: '100%' }}>
-          <div id="dg-innerContent" style={{ height: '100%' }}>
+      <SideBar name={name} sortedPages={sortedPages} pdfEnabled={pdfEnabled} />
+      <Page>
+        <section id="dg-content">
+          <div id="dg-innerContent">
             {showPdf ? (
-              <PDFViewer width="100%" height="800">
-                <Pdf
-                  parameters={parameters}
-                  options={{}}
-                  sortedPages={sortedPages}
-                />
+              <PDFViewer width="100%" height={800}>
+                <Pdf parameters={parameters} options={{}} sortedPages={sortedPages} />
               </PDFViewer>
             ) : (
               <Outlet />
-              )}
+            )}
           </div>
         </section>
         <WebFooter parameters={parameters} />
-      </PageLayout>
+      </Page>
     </>
   );
 };
