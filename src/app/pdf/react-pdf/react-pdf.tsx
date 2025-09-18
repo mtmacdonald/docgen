c9d458a9b369ca470c0e13eb01384f35b4f5a0d2
Mark Macdonald <mark.t.macdonald@googlemail.com>
2025-09-18 20:03:17 +0100

    Fix PDF fonts

--- src/app/pdf/react-pdf/react-pdf.tsx
+++ src/app/pdf/react-pdf/react-pdf.tsx
@@ -3,31 +3,44 @@ import { Document, Font } from '@react-pdf/renderer';
 import { PdfPage } from './pdf-page/pdf-page.tsx';
 import { marked } from 'marked';
 
+import archivoRegular from '../../views/assets/styles/fonts/archivo-regular.ttf';
+import archivoItalic from '../../views/assets/styles/fonts/archivo-italic.ttf';
+import archivo600 from '../../views/assets/styles/fonts/archivo-600.ttf';
+import archivo600Italic from '../../views/assets/styles/fonts/archivo-600-italic.ttf';
+
+import spaceGroteskRegular from '../../views/assets/styles/fonts/space-grotesk-regular.ttf';
+import spaceGrotesk600 from '../../views/assets/styles/fonts/space-grotesk-600.ttf';
+
+import spaceMonoRegular from '../../views/assets/styles/fonts/space-mono-regular.ttf';
+import spaceMonoItalic from '../../views/assets/styles/fonts/space-mono-italic.ttf';
+import spaceMono700 from '../../views/assets/styles/fonts/space-mono-700.ttf';
+import spaceMono700Italic from '../../views/assets/styles/fonts/space-mono-700-italic.ttf';
+
 Font.register({
   family: 'archivo',
   fonts: [
-    { src: '/assets/archivo-regular.ttf', fontStyle: 'normal', fontWeight: 400 },
-    { src: '/assets/archivo-italic.ttf', fontStyle: 'italic', fontWeight: 400 },
-    { src: '/assets/archivo-600.ttf', fontStyle: 'normal', fontWeight: 600 },
-    { src: '/assets/archivo-600-italic.ttf', fontStyle: 'italic', fontWeight: 600 },
+    { src: archivoRegular, fontStyle: 'normal', fontWeight: 400 },
+    { src: archivoItalic, fontStyle: 'italic', fontWeight: 400 },
+    { src: archivo600, fontStyle: 'normal', fontWeight: 600 },
+    { src: archivo600Italic, fontStyle: 'italic', fontWeight: 600 },
   ],
 });
 
 Font.register({
   family: 'space-grotesk',
   fonts: [
-    { src: '/assets/space-grotesk-regular.ttf', fontStyle: 'normal', fontWeight: 400 },
-    { src: '/assets/space-grotesk-600.ttf', fontStyle: 'normal', fontWeight: 600 },
+    { src: spaceGroteskRegular, fontStyle: 'normal', fontWeight: 400 },
+    { src: spaceGrotesk600, fontStyle: 'normal', fontWeight: 600 },
   ],
 });
 
 Font.register({
   family: 'space-mono',
   fonts: [
-    { src: '/assets/space-mono-regular.ttf', fontStyle: 'normal', fontWeight: 400 },
-    { src: '/assets/space-mono-italic.ttf', fontStyle: 'italic', fontWeight: 400 },
-    { src: '/assets/space-mono-700.ttf', fontStyle: 'normal', fontWeight: 700 },
-    { src: '/assets/space-mono-700-italic.ttf', fontStyle: 'italic', fontWeight: 700 },
+    { src: spaceMonoRegular, fontStyle: 'normal', fontWeight: 400 },
+    { src: spaceMonoItalic, fontStyle: 'italic', fontWeight: 400 },
+    { src: spaceMono700, fontStyle: 'normal', fontWeight: 700 },
+    { src: spaceMono700Italic, fontStyle: 'italic', fontWeight: 700 },
   ],
 });
 
