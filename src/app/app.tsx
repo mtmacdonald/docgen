cf8590383076f2234f8946441d0b34ee872f0697
Mark Macdonald <mark.t.macdonald@googlemail.com>
2025-09-24 18:10:48 +0100

    Remove ts-ignore

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -6,7 +6,11 @@ import './views/assets/styles/framework.css';
 
 const app = () => {
   const rootElement = document.getElementById('root');
-  // @ts-ignore
+
+  if (!rootElement) {
+    throw new Error('Root element not found');
+  }
+
   const root = ReactDOM.createRoot(rootElement);
   root.render(<RouterProvider router={router} />);
 };
