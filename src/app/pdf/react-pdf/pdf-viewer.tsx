dc29d58418367e13ee9190882ceac3713c4a9903
Mark Macdonald <mark.t.macdonald@googlemail.com>
2025-11-01 15:33:04 +0000

    Hello world new PDF viewer

--- src/app/pdf/react-pdf/pdf-viewer.tsx
+++ src/app/pdf/react-pdf/pdf-viewer.tsx
@@ -1,55 +1,77 @@
-import React from 'react';
-import { BlobProvider, PDFViewer as ReactPDFViewer } from '@react-pdf/renderer';
-import { Pdf } from './react-pdf.tsx';
+import React, { useState, useEffect, useLayoutEffect, useMemo } from 'react';
+import { usePDF, PDFViewer as ReactPDFViewer } from '@react-pdf/renderer';
 import { Document, Page, pdfjs } from 'react-pdf';
 import type { TParameters, TSortedPages } from '../../../docgen/types.ts';
+import { Pdf } from './react-pdf.tsx';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString();
+
 import 'react-pdf/dist/Page/TextLayer.css';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
-
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 declare const __DOCGEN_PARAMETERS__: TParameters;
 declare const __DOCGEN_PAGES__: TSortedPages;
 
-const enableWorkaround = false;
+const enableWorkaround = true;
+
+const PDF = <Pdf parameters={__DOCGEN_PARAMETERS__} options={{}} sortedPages={__DOCGEN_PAGES__} />;
 
 export const PDFViewer = () => {
-  if (enableWorkaround) {
-    console.log('got here');
-    return (
-      <BlobProvider
-        document={<Pdf parameters={__DOCGEN_PARAMETERS__} options={{}} sortedPages={__DOCGEN_PAGES__} />}
-      >
-        {({ blob, url, loading }) => {
-          if (loading) {
-            return (
-              <div>Loading...</div>
-            );
-          }
-          return (
-            <div style={{ width: '100%' }}>
-              <Document
-                file={url}
-                onLoadSuccess={(pdf) => console.log('Loaded PDF:', pdf.numPages)}
-                renderMode="canvas"
-              >
-                {Array.from(new Array(1), (_, index) => (
-                  <Page key={`page_${index + 1}`} pageNumber={index + 1} width={window.innerWidth} />
-                ))}
-              </Document>
-            </div>
-          );
+  const [instance] = usePDF({ document: PDF });
+  const [fileData, setFileData] = useState<Uint8Array | null>(null);
+  const [numPages, setNumPages] = useState(0);
+  const [pdfKey, setPdfKey] = useState(0);
+
+  useLayoutEffect(() => {
+    setPdfKey((prevKey) => prevKey + 1);
+  }, [instance.blob]);
+
+  useEffect(() => {
+    if (instance.blob) {
+      instance.blob.arrayBuffer().then(buffer => {
+        setFileData(new Uint8Array(buffer)); // keep as Uint8Array
+      });
+    }
+  }, [instance.blob]);
+  console.log(fileData);
+
+  const render = useMemo(() => (
+    fileData &&
+    <div style={{ width: '100%' }}>
+      <Document
+        key={pdfKey}
+        file={{ data: fileData }}
+        onLoadSuccess={({ numPages }) => {
+          console.log(numPages);
+          setNumPages(numPages)
         }}
-      </BlobProvider>
-    );
+        renderMode="canvas"
+      >
+        <Page
+          pageNumber={1}
+          width={window.innerWidth}
+        />
+      </Document>
+    </div>
+  ), [fileData]);
+
+  if (instance.loading || !fileData) return <div>Loading ...</div>;
+  if (instance.error) return <div>Something went wrong: {instance.error}</div>;
+
+  if (enableWorkaround) {
+    return render;
   }
+
   return (
-    <ReactPDFViewer width="100%" height={800} >
+    <ReactPDFViewer width="100%" height={800}>
       <Pdf
         parameters={__DOCGEN_PARAMETERS__}
         options={{}}
         sortedPages={__DOCGEN_PAGES__}
       />
     </ReactPDFViewer>
-  )
+  );
 };
