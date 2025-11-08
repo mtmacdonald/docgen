3d235faeada57064c85585cb55cf54fd4ebfaca2
Mark Macdonald <mark.t.macdonald@googlemail.com>
2025-11-08 17:11:00 +0000

    Switch to forked version of react-pdf-html with css-tree upgrade

--- src/app/pdf/react-pdf/pdf-page/pdf-html-block/pdf-html-block.tsx
+++ src/app/pdf/react-pdf/pdf-page/pdf-html-block/pdf-html-block.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Html from 'react-pdf-html';
+import Html from 'react-pdf-html-simple';
 import { fontSize, htmlStyleSheet } from '../../pdf-styles/pdf-styles.ts';
 import { customRenderers } from "./custom-renderers/custom-renderers.tsx";
 
